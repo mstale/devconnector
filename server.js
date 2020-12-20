f7f9770539f84f7bda4f59e0cb8600a059f6593d
Sadegh Tale <ms.taleh@gmail.com>
2020-12-20 21:17:31 +0330

    test github connectivity

--- server.js
+++ server.js
@@ -7,3 +7,5 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`---> Server is running on port ${PORT}`));
 
 app.get('/', (req, res) => { res.send('API running') });
+
+// test for github
