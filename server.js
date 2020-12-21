4e3ceef328fa868de3466288b2759fa0963d07d6
Sadegh Tale <ms.taleh@gmail.com>
2020-12-21 20:01:41 +0330

    Setup of apit routes and structure

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require('express');
+const connectDB = require('./config/db.js');
 
+// initialize server and database
 const app = express();
+connectDB();
 
+// Server Listening to PORT
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => console.log(`---> Server is running on port ${PORT}`));
 
-app.get('/', (req, res) => { res.send('API running') });
+// Define Routes
+app.use('/api/users', require('./routes/api/users.js'));
+app.use('/api/auth', require('./routes/api/auth.js'));
+app.use('/api/profile', require('./routes/api/profile.js'));
+app.use('/api/posts', require('./routes/api/posts.js'));
 
-// test for github
+
+// Test api get path
+app.get('/', (req, res) => { res.send('API running') });
